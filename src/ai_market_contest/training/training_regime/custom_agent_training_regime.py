6b7cdd3e5233f59a159ac579ebdf455f4932a1ca
Matteo Bongiovanni <40599507+MatBon01@users.noreply.github.com>
2023-11-15 19:22:33 +0000

    Refactor simulation out of epochs counter

--- src/ai_market_contest/training/training_regime/custom_agent_training_regime.py
+++ src/ai_market_contest/training/training_regime/custom_agent_training_regime.py
@@ -14,6 +14,7 @@ from ai_market_contest.cli.utils.existing_agent.existing_agent_version import (
     ExistingAgentVersion,
 )
 from ai_market_contest.cli.utils.training import get_agent_price_dict
+from ai_market_contest.environment import Market
 from ai_market_contest.training.agent_name_maker import AgentNameMaker
 from ai_market_contest.training.sequential_agent_name_maker import (
     SequentialAgentNameMaker,
@@ -37,44 +38,23 @@ class CustomAgentTrainingRegime(TrainingRegime):
         self.agent_config_reader = agent_config_reader
 
     def execute(self) -> None:
-        # Assumes agent to train is always first in the list
         agent_name_maker: AgentNameMaker = SequentialAgentNameMaker(
             self.training_config_reader.get_num_agents()
         )
-        env = self.training_config_reader.get_environment(agent_name_maker)
+        env: Market = self.training_config_reader.get_environment(agent_name_maker)
 
-        self_play_agents: list[Agent] = self.training_config_reader.get_self_play_agents(
-            self.agent_version
+        # Assumes agent to train is always first in the list
+        agents: list[Agent]
+        self_play_agents: list[Agent]
+        agents, self_play_agents, _, _ = self._get_agents_in_simulation(
+            env, agent_name_maker
         )
-        naive_agents: list[Agent] = self.training_config_reader.get_naive_agents()
-        trained_agents: list[Union[Agent, Trainer]] = self.training_config_reader.get_trained_agents(
-            self.project_dir, env
-        )
-
-        trained_agents: list[Agent] = list(map(lambda agent: TrainerAgentAdapter.convert_if_trainer(agent, agent_name_maker.get_names()), trained_agents))
-
-        agents: list[Agent] = []
-        agents.extend(self_play_agents + naive_agents)
-        agents.extend(trained_agents)
 
         epochs = self.training_config_reader.get_epochs()
         cumulative_profits: list[int] = []
         for epoch in range(epochs):
-            current_prices: dict[str, int] = {}
-
-            for agent, agent_name in zip(agents, env.agents):
-                    current_prices[agent_name] = agent.get_initial_price()
-
-            cumulative_profit = 0
-            for _ in range(env.simulation_length):
-                current_prices = get_agent_price_dict(agents, env, current_prices)
-                _, rewards, _, _ = env.step(current_prices)
-                for index, agent in enumerate(self_play_agents):
-                    agent.update(rewards[env.agents[index]], index)
-
-                cumulative_profit += rewards[env.agents[0]]
-
-            cumulative_profits.append(cumulative_profit)
+            profit: int = self._simulation(agents, env, self_play_agents)
+            cumulative_profits.append(profit)
 
             if self.training_config_reader.print_training():
                 status = "epoch {:2d} \nreward min: {:6.2f}\nreward mean: {:6.2f}\nreward max:  {:6.2f}\nmean length: {:4.2f}\n"
@@ -89,8 +69,56 @@ class CustomAgentTrainingRegime(TrainingRegime):
                 )
 
         save_new_custom_agent(
-            self_play_agents[0],
+            agents[0],
             self.agent_version,
             self.training_msg,
             self.training_config_reader.get_config_file_path(),
         )
+
+    def _get_agents_in_simulation(
+        self, environment: Market, agent_name_maker: AgentNameMaker
+    ) -> tuple[list[Agent], list[Agent], list[Agent], list[Agent]]:
+        self_play_agents: list[
+            Agent
+        ] = self.training_config_reader.get_self_play_agents(self.agent_version)
+        naive_agents: list[Agent] = self.training_config_reader.get_naive_agents()
+        trained_agents: list[
+            Union[Agent, Trainer]
+        ] = self.training_config_reader.get_trained_agents(
+            self.project_dir, environment
+        )
+
+        trained_agents: list[Agent] = list(
+            map(
+                lambda agent: TrainerAgentAdapter.convert_if_trainer(
+                    agent, agent_name_maker.get_names()
+                ),
+                trained_agents,
+            )
+        )
+
+        agents: list[Agent] = []
+        agents.extend(self_play_agents)
+        agents.extend(naive_agents)
+        agents.extend(trained_agents)
+
+        return agents, self_play_agents, naive_agents, trained_agents
+
+    def _simulation(
+        self, agents: list[Agent], environment: Market, self_play_agents: list[Agent]
+    ) -> int:
+        current_prices: dict[str, int] = {}
+
+        for agent, agent_name in zip(agents, environment.agents):
+            current_prices[agent_name] = agent.get_initial_price()
+
+        cumulative_profit = 0
+        for _ in range(environment.simulation_length):
+            current_prices = get_agent_price_dict(agents, environment, current_prices)
+            _, rewards, _, _ = environment.step(current_prices)
+            for index, agent in enumerate(self_play_agents):
+                agent.update(rewards[environment.agents[index]], index)
+
+            cumulative_profit += rewards[environment.agents[0]]
+
+        return cumulative_profit
